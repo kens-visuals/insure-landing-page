cc0be87946070dd7734bc431d65fa9312b12cf73
Ken Nersisyan <karapet.nersisyan@yahoo.com>
2021-12-16 11:04:04 -0800

    minor change in JS

--- js/script.js
+++ js/script.js
@@ -1,16 +1,18 @@
 const nav = document.querySelector('.js-nav');
 const hamburgerBtn = document.querySelector('.js-hamburger-btn');
 
-hamburgerBtn.addEventListener('click', () => {
+const toggleMenu = function () {
   if (!nav.classList.contains('nav--is-open')) {
     nav.classList.add('nav--is-open');
-    document.body.classList.add('is-fixed');
+    document.body.classList.add('body--is-fixed');
     hamburgerBtn.setAttribute('aria-expanded', true);
     hamburgerBtn.firstElementChild.src = './images/icon-close.svg';
   } else {
     nav.classList.remove('nav--is-open');
-    document.body.classList.remove('is-fixed');
+    document.body.classList.remove('body--is-fixed');
     hamburgerBtn.setAttribute('aria-expanded', false);
     hamburgerBtn.firstElementChild.src = './images/icon-hamburger.svg';
   }
-});
+};
+
+hamburgerBtn.addEventListener('click', toggleMenu);
